58c9c68eafcf9d5205746d4e45b979cfe8187857
Oxana Kraynova <oxana.kraynova@dtforce.com>
2024-06-04 10:54:15 +0200

    - fix center items md and below

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,35 +2,33 @@ import heroImage from "../assets/image6.png";
 
 function HeroSection() {
   return (
-    <>
-      <section className="flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-between space-y-8 text-center lg:text-left">
-        <div className="mr-auto place-self-center">
-          <h3 className="max-w-2xl pb-7 text-lg text-primary font-bold tracking-tight leading-none md:text-xl xl:text-2xl">
-            EYEBROW IPSUM
-          </h3>
-          <h1 className="max-w-2xl font-bold text-3xl text-secondary md:text-4xl lg:text-5xl">
-            Lorem ipsum
-          </h1>
-          <h3 className="max-w-2xl hidden lg:block text-lg mt-10 text-secondary font-medium tracking-tight leading-none md:text-xl xl:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
-            tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi.
-          </h3>
+    <section className="flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-between space-y-8 text-center lg:text-left">
+      <div className="lg:mr-auto place-self-center">
+        <h3 className="max-w-2xl pb-7 text-lg text-primary font-bold tracking-tight leading-none md:text-xl xl:text-2xl">
+          EYEBROW IPSUM
+        </h3>
+        <h1 className="max-w-2xl font-bold text-3xl text-secondary md:text-4xl lg:text-5xl">
+          Lorem ipsum
+        </h1>
+        <h3 className="max-w-2xl hidden lg:block text-lg mt-10 text-secondary font-medium tracking-tight leading-none md:text-xl xl:text-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi.
+        </h3>
+      </div>
+      <div className="md:flex">
+        <div className="md:shrink-0 md:place-self-center">
+          <img src={heroImage} alt="shop image" />
         </div>
-        <div className="md:flex ">
-          <div className="md:shrink-0 md:place-self-center">
-            <img src={heroImage} alt="shop image" />
-          </div>
-        </div>
-        <div>
-          <h3 className="max-w-2xl lg:hidden block md:pt-10 sm:pt-9 text-lg text-secondary font-medium tracking-tight leading-none md:text-xl xl:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
-            tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi.
-          </h3>
-        </div>
-      </section>
-    </>
+      </div>
+      <div>
+        <h3 className="max-w-2xl lg:hidden block md:pt-10 sm:pt-9 text-lg text-secondary font-medium tracking-tight leading-none md:text-xl xl:text-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi.
+        </h3>
+      </div>
+    </section>
   );
 }
 
